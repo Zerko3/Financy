64f2b8876e475e785d2c426fb9672988975f0f53
Žan Žerak <zan.zerak99@gmail.com>
2023-08-16 13:42:54 +0200

    adding login

--- src/app/login-component/login-component.component.ts
+++ src/app/login-component/login-component.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-component',
   templateUrl: './login-component.component.html',
-  styleUrls: ['./login-component.component.scss']
+  styleUrls: ['./login-component.component.scss'],
 })
 export class LoginComponentComponent {
-
+  constructor(private router: Router) {}
+  userHeroNavigation() {
+    this.router.navigate(['']);
+  }
 }
