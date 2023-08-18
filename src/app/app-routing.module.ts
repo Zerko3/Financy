34a7e4962888ec8fcea1ef22986a0497b99726f0
Žan Žerak <zan.zerak99@gmail.com>
2023-08-18 13:35:35 +0200

    adding navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,24 @@ import { HeroComponentComponent } from './hero-component/hero-component.componen
 import { LoginComponentComponent } from './login-component/login-component.component';
 import { RegisterComponentComponent } from './register-component/register-component.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ExpenseComponent } from './dashboard/expense/expense.component';
+import { InvestingComponent } from './dashboard/investing/investing.component';
+import { SaveingsComponent } from './dashboard/saveings/saveings.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'hero', pathMatch: 'full' },
   { path: 'hero', component: HeroComponentComponent },
   { path: 'login', component: LoginComponentComponent },
   { path: 'register', component: RegisterComponentComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [
+      { path: 'expense', component: ExpenseComponent },
+      { path: 'investing', component: InvestingComponent },
+      { path: 'saveings', component: SaveingsComponent },
+    ],
+  },
 ];
 
 @NgModule({
--- src/app/dashboard/navigation/navigation.component.ts
+++ src/app/dashboard/navigation/navigation.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent {
+  constructor(private router: Router) {}
 
+  userNavigate(e: HTMLAnchorElement) {
+    if (e.innerText === 'Overview') {
+      this.router.navigate(['dashboard']);
+    }
+
+    if (e.innerText === 'Investing') {
+      this.router.navigate(['dashboard/investing']);
+    }
+
+    if (e.innerText === 'Monthly expenses') {
+      this.router.navigate(['dashboard/expense']);
+    }
+
+    if (e.innerText === 'Saveings') {
+      this.router.navigate(['dashboard/saveings']);
+    }
+  }
 }
