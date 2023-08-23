3c860ec97c7f06c1d4d5c42e9078db3037ef0df8
Žan Žerak <zan.zerak99@gmail.com>
2023-08-23 14:38:52 +0200

    adding styles and layout

--- src/app/dashboard/expense/expense.component.ts
+++ src/app/dashboard/expense/expense.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component } from '@angular/core';
 import { NavigationService } from 'src/services/navigation.service';
 
 @Component({
@@ -8,6 +7,12 @@ import { NavigationService } from 'src/services/navigation.service';
   styleUrls: ['./expense.component.scss'],
 })
 export class ExpenseComponent {
+  // TODO:
+  // 1. model or interface?
+  // 2. service to store the data for now
+  // 3. later on call the data from firebase
+  // 4. Service will get and post this valid data
+  // 5. give or recive data from service
   expenses = [
     {
       Date: new Date(),
@@ -15,8 +20,25 @@ export class ExpenseComponent {
       Expanses: 'Subscription',
       Name: 'Apple',
       Status: 'Paid',
+      Account: 'Paypal',
+    },
+    {
+      Date: new Date(),
+      Money: 100,
+      Expanses: 'Subscription',
+      Name: 'Apple',
+      Status: 'Paid',
+      Account: 'Paypal',
+    },
+    {
+      Date: new Date(),
+      Money: 100,
+      Expanses: 'Subscription',
+      Name: 'Apple',
+      Status: 'Paid',
+      Account: 'Paypal',
     },
   ];
-  columns = ['Date', 'Money', 'Expanses', 'Name', 'Status'];
+  columns = ['Date', 'Money', 'Expanses', 'Name', 'Status', 'Account'];
   constructor(private navigationService: NavigationService) {}
 }
