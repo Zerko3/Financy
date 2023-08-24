25a0d928b92b7cba50c6a2adcb18f0de89e740a9
Žan Žerak <zan.zerak99@gmail.com>
2023-08-24 09:57:10 +0200

    adding styles to expanses

--- src/app/dashboard/expense/expense.component.ts
+++ src/app/dashboard/expense/expense.component.ts
@@ -7,13 +7,84 @@ import { NavigationService } from 'src/services/navigation.service';
   styleUrls: ['./expense.component.scss'],
 })
 export class ExpenseComponent {
+  allowedPageSizes = [10, 'all'];
+  displayMode = 'full';
+  showPageSizeSelector = true;
+  showInfo = true;
+  showNavButtons = true;
   // TODO:
   // 1. model or interface?
   // 2. service to store the data for now
   // 3. later on call the data from firebase
   // 4. Service will get and post this valid data
   // 5. give or recive data from service
+
+  // WILL REMOVE
   expenses = [
+    {
+      Date: new Date(),
+      Money: 100,
+      Expanses: 'Subscription',
+      Name: 'Netflix',
+      Status: 'Paid',
+      Account: 'Paypal',
+    },
+    {
+      Date: new Date(),
+      Money: 100,
+      Expanses: 'Subscription',
+      Name: 'Apple',
+      Status: 'Paid',
+      Account: 'Paypal',
+    },
+    {
+      Date: new Date(),
+      Money: 100,
+      Expanses: 'Subscription',
+      Name: 'Apple',
+      Status: 'Paid',
+      Account: 'Paypal',
+    },
+    {
+      Date: new Date(),
+      Money: 100,
+      Expanses: 'Subscription',
+      Name: 'Apple',
+      Status: 'Paid',
+      Account: 'Paypal',
+    },
+    {
+      Date: new Date(),
+      Money: 100,
+      Expanses: 'Subscription',
+      Name: 'Apple',
+      Status: 'Paid',
+      Account: 'Paypal',
+    },
+    {
+      Date: new Date(),
+      Money: 100,
+      Expanses: 'Subscription',
+      Name: 'Apple',
+      Status: 'Paid',
+      Account: 'Paypal',
+    },
+    {
+      Date: new Date(),
+      Money: 100,
+      Expanses: 'Subscription',
+      Name: 'Apple',
+      Status: 'Paid',
+      Account: 'Paypal',
+    },
+    {
+      Date: new Date(),
+      Money: 100,
+      Expanses: 'Subscription',
+      Name: 'Apple',
+      Status: 'Paid',
+      Account: 'Paypal',
+    },
     {
       Date: new Date(),
       Money: 100,
@@ -41,4 +112,18 @@ export class ExpenseComponent {
   ];
   columns = ['Date', 'Money', 'Expanses', 'Name', 'Status', 'Account'];
   constructor(private navigationService: NavigationService) {}
+
+  // TODO:
+  // 1. Get valid row
+  // 2. Get valid data and pass on the color
+  // 3. Add multiple color options for paid unpaid subscription food ,....
+  cellStyle(e) {
+    if (e.rowType === 'data' && e.column.dataField === 'Status') {
+      e.cellElement.classList.add('test');
+      // e.cellElement.style.color = 'green';
+      // e.cellElement.style.fontWeight = 'bold';
+
+      // e.cellElement.addClass = 'test';
+    }
+  }
 }
